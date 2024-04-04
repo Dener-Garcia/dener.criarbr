98a80a138eef1056dd34343fde4f242597d714a7
Dener <dener.criarbr@gmail.com>
2024-04-04 16:57:55 -0300

    fix: vite.config.js adress nested pages

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // comment base line for deploy out github pages
- base: '/site-portfolio-dener-criarbr',
+ //base: '/site-portfolio-dener-criarbr',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-
-        nested: resolve(__dirname, '/src/pages/pop-cinema.html'),
-
-        main: resolve(__dirname, 'index.html'),
+        autista_live: resolve(__dirname, '/src/pages/autista-live.html'),
         pop_cinema: resolve(__dirname, '/src/pages/pop-cinema.html'),
         lit_academy: resolve(__dirname, '/src/pages/lit-academy.html'),
         company_geradores: resolve(__dirname, '/src/pages/company-geradores.html'),
